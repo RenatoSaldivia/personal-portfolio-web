7254b9bcf72810224c7a9b74213fa34e823043fd
RenatoSaldivia <ren.saldiviap@gmail.com>
2026-09-10 16:28:32 -0300

    ci: configure automated Astro build and deployment via GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://renatosaldivia.github.io',
+  base: '/personal-portfolio-web',
+});
